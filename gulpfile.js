1f5970e4a8e688b46a00d8dbe67b9e5ec14a0188
gifteconomist <gracepoetzinger@gmail.com>
2017-01-29 18:27:12 -0500

    npm init

--- gulpfile.js
+++ gulpfile.js
@@ -3,43 +3,45 @@ var gulp = require('gulp'),
     postcss = require('gulp-postcss'), //handles css plugins like autoprefixer and cssnano
     autoprefixer = require('autoprefixer'), //autoprefixes css
     cssnano = require('cssnano'), //minifies css
-	  sourcemaps = require('gulp-sourcemaps'), //maps css to sass file in the DOM
+    sourcemaps = require('gulp-sourcemaps'), //maps css to sass file in the DOM
     concat = require('gulp-concat'), //concatenates js into one file
     uglify = require('gulp-uglify'), //minifies js
     pug = require('gulp-pug'), //plugin to compile pug (renamed from jade) templates
-  	imagemin = require('gulp-imagemin'), //optimize images
-	  cache = require('gulp-cache'), //caches images so that minified images dont get reprocessed
+    imagemin = require('gulp-imagemin'), //optimize images
+    cache = require('gulp-cache'), //caches images so that minified images dont get reprocessed
     bs = require('browser-sync').create();
 
 
 
- // SASS
+// SASS
 
 var sassSrc = ['src/sass/*.scss'],
-	  sassDist = ['public/css'];
+    sassDist = ['public/css'];
 
-gulp.task('css', function () {
+gulp.task('css', function() {
     var processors = [
-        autoprefixer(),
-        cssnano(),
-    ],
-  	sassOptions = {
-  	  errLogToConsole: true,
-  	  outputStyle: 'compressed'
-  	};
+            autoprefixer(),
+            cssnano(),
+        ],
+        sassOptions = {
+            errLogToConsole: true,
+            outputStyle: 'compressed'
+        };
     return gulp.src(sassSrc)
-		.pipe(sourcemaps.init())
+        .pipe(sourcemaps.init())
         .pipe(sass(sassOptions))
         .pipe(postcss(processors))
-		    .pipe(sourcemaps.write('./maps'))
+        .pipe(sourcemaps.write('./maps'))
         .pipe(gulp.dest('public/css'))
-        .pipe(bs.reload({stream: true}));
+        .pipe(bs.reload({
+            stream: true
+        }));
 });
 
 // JAVASCRIPT
 
 var jsSrc = ['src/scripts/*.js'],
-	  jsDist = ['public/js'];
+    jsDist = ['public/js'];
 
 gulp.task('js', function() {
     gulp.src(jsSrc)
@@ -55,28 +57,28 @@ var pugIndexSrc = ['src/pug/*.pug'],
 
 
 gulp.task('pugIndex', function() {
-   return gulp.src(pugIndexSrc)
-     .pipe(pug())
-     .pipe(gulp.dest(''));
+    return gulp.src(pugIndexSrc)
+        .pipe(pug())
+        .pipe(gulp.dest(''));
 });
 
 gulp.task('pugIncludes', function() {
-   return gulp.src(pugIncludesSrc)
-     .pipe(pug())
-     .pipe(gulp.dest('includes'));
+    return gulp.src(pugIncludesSrc)
+        .pipe(pug())
+        .pipe(gulp.dest('includes'));
 });
 
 gulp.task('pug', ['pugIndex', 'pugIncludes']);
 
 // ASSETS
 
-gulp.task('images', function(){
-   return gulp.src('src/images/**/*.+(png|jpg|jpeg|gif|svg)')
-   .pipe(cache(imagemin({
-       interlaced: true
-     })))
-   .pipe(imagemin())
-   .pipe(gulp.dest('public/img'))
+gulp.task('images', function() {
+    return gulp.src('src/images/**/*.+(png|jpg|jpeg|gif|svg)')
+        .pipe(cache(imagemin({
+            interlaced: true
+        })))
+        .pipe(imagemin())
+        .pipe(gulp.dest('public/img'))
 });
 
 // LIVE RELOAD
@@ -91,10 +93,10 @@ gulp.task('browser-sync', function() {
 
 // WATCH
 
-gulp.task('watch', ['browser-sync'] function() {
+gulp.task('watch', ['browser-sync']function() {
     gulp.watch(sassSrc, ['css']);
     gulp.watch(jsSrc, ['js']);
-	  gulp.watch([pugIndexSrc, pugIncludesSrc], ['pug']);
+    gulp.watch([pugIndexSrc, pugIncludesSrc], ['pug']);
 });
 
 
